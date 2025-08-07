d25dea0d3a46141446a88814bd172876dd0924f6
Angel Sanchez <percycsa@hotmail.com>
2025-08-07 03:43:07 +0000

    no se los banner

--- src/components/ad-banner.tsx
+++ src/components/ad-banner.tsx
@@ -28,7 +28,11 @@ export const AdBanner = () => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full justify-center items-center bg-muted/30 rounded-lg overflow-hidden p-2">
             <div className="flex justify-center items-center min-h-[50px]">
-                <Script src="https://c.bannerflow.net/a/66d093873ec9f495810c7166?did=657fff592225a91f2b2e2296&deeplink=on&adgroupid=66d093873ec9f495810c716b&redirecturl=https://record.betsson.com/_JVOvJkxHMMsyGSrLOh2Z726ly77WL6Qu/1/&media=208756&campaign=1" async />
+               <iframe
+                    src="https://c.bannerflow.net/a/66d093873ec9f495810c7166?did=657fff592225a91f2b2e2296&deeplink=on&adgroupid=66d093873ec9f495810c716b&redirecturl=https://record.betsson.com/_JVOvJkxHMMsyGSrLOh2Z726ly77WL6Qu/1/&media=208756&campaign=1"
+                    scrolling="no"
+                    style={{ border: 'none', width: '300px', height: '250px', overflow: 'hidden' }}
+                ></iframe>
             </div>
             
             <div className="flex justify-center items-center min-h-[50px]">
@@ -43,7 +47,11 @@ export const AdBanner = () => {
             </div>
 
             <div className="flex justify-center items-center min-h-[50px]">
-                <Script src="https://c.bannerflow.net/a/66a379674626d28804982c70?did=657fff592225a91f2b2e2296&deeplink=on&adgroupid=66a379674626d28804982c75&redirecturl=https://record.inkabet.pe/_JVOvJkxHMMsYx3xzOqdcQ_tY1Quv5xos/1/&media=208596&campaign=1" async />
+                <iframe
+                    src="https://c.bannerflow.net/a/66a379674626d28804982c70?did=657fff592225a91f2b2e2296&deeplink=on&adgroupid=66a379674626d28804982c75&redirecturl=https://record.inkabet.pe/_JVOvJkxHMMsYx3xzOqdcQ_tY1Quv5xos/1/&media=208596&campaign=1"
+                    scrolling="no"
+                    style={{ border: 'none', width: '300px', height: '250px', overflow: 'hidden' }}
+                ></iframe>
             </div>
         </div>
     );
