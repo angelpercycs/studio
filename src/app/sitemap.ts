d25c295159fdf86072514a42f8e3b9f7377f9c1c
Angel Sanchez <percycsa@hotmail.com>
2025-12-11 12:50:53 +0000

    sobre https://www.fszscore.com/sitemap.xml como se elimina la url inexis

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,11 +4,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://www.fszscore.com';
 
   const routes = [
-    { url: '/pronostico-del-dia/hoy', changeFrequency: 'daily', priority: 1.0 },
-    { url: '/pronostico-del-dia/ayer', changeFrequency: 'daily', priority: 0.9 },
-    { url: '/pronostico-del-dia/mañana', changeFrequency: 'daily', priority: 0.9 },
-    { url: '/pronostico-de-la-semana', changeFrequency: 'weekly', priority: 0.8 },
-    { url: '/', changeFrequency: 'hourly', priority: 0.7 },
+    { url: '/', changeFrequency: 'hourly', priority: 1.0 },
   ];
 
   return routes.map((route) => ({
