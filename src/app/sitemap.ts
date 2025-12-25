152086568697f7629586876b3779abeb173ebcc3
Angel Sanchez <percycsa@hotmail.com>
2025-12-25 00:03:07 +0000

    porfavor en el sitemap quita las www. en todas la direciones actualiza s

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.fszscore.com';
+  const baseUrl = 'https://fszscore.com';
 
   const routes = [
     { url: '/partidos/hoy', changeFrequency: 'hourly', priority: 1.0 },
