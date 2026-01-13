a4bbbdf0489a80bd9ee09c2727d9c58216197c0a
Angel Sanchez <percycsa@hotmail.com>
2026-01-13 17:10:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "match-tracker-fdmau",
+  "appId": "1:456722785456:web:4adb6ed5139b758f678775",
+  "apiKey": "AIzaSyD7rA7TE5B-10q4Y9ZwzSTh1KxiYYqeQQA",
+  "authDomain": "match-tracker-fdmau.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "456722785456"
+};
