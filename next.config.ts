a71c2dfa2d2ca70a35f2934ce0c4bb660c93e1b6
Angel Sanchez <percycsa@hotmail.com>
2026-01-23 02:51:54 +0000

    son instruciones Step 2: Ads.txt Setup Ads.txt setup is required for all

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/ads.txt',
+        destination: 'https://srv.adstxtmanager.com/19390/fszscore.com',
+        permanent: true,
+      },
+    ]
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
