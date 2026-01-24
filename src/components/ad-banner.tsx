143af9069b07a82ed0a42df0af9bb7096f195912
Angel Sanchez <percycsa@hotmail.com>
2026-01-24 03:33:09 +0000

    borra todo banner que no quede nada de nada ni temu ni nada borra to de

--- src/components/ad-banner.tsx
+++ src/components/ad-banner.tsx
@@ -1,25 +1,5 @@
 "use client";
 
-import { useUserProfile } from '@/hooks/use-user-profile';
-import { Skeleton } from './ui/skeleton';
-
 export const AdBanner = () => {
-    const { isDonor, isLoading } = useUserProfile();
-
-    if (isLoading) {
-        return (
-            <div className="flex flex-col md:flex-row justify-center items-center gap-4 w-full p-2">
-                <Skeleton className="w-[320px] h-[50px]" />
-                <Skeleton className="w-[300px] h-[250px]" />
-            </div>
-        );
-    }
-    
-    if (isDonor) {
-        return null;
-    }
-
-    // Ads are currently disabled as per user request.
-    // Returning null until Ezoic is fully approved and ads are re-enabled.
     return null;
 };
